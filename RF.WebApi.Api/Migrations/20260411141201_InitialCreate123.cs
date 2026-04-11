d534e83713f0aaa4e6d65970105b6592a6077cdd
HP <HP@DESKTOP-3TJ85N2>
2026-04-11 19:45:40 +0530

    feat: add AccountId column to contribution tables and make AccountPersonId nullable

---
+++ RF.WebApi.Api/Migrations/20260411141201_InitialCreate123.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RF.WebApi.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate123 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountPersonId",
+                table: "RemoveContribution",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "RemoveContribution",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountPersonId",
+                table: "AddContribution",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "AddContribution",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "Password",
+                value: "$2a$11$/Hu.ql.8Dfl7TQMDgBnxtu40fGgeMvX3kVv1Oijb/IdQ94.44qFK6");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "RemoveContribution");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "AddContribution");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountPersonId",
+                table: "RemoveContribution",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccountPersonId",
+                table: "AddContribution",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "Password",
+                value: "$2a$11$xW9WstIUuodWu9UFPABk/OIxeWNdd9YHPdNqay7VyYeC.1YuL6Ai2");
+        }
+    }
+}
